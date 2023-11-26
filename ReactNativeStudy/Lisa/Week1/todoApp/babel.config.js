40f651e7c07209ed1e60812c78a73d5f7b7934d3
ghdtjgus76 <ghdtjgus76@naver.com>
2023-11-27 06:43:55 +0900

    feat: 절대 경로 세팅

--- ReactNativeStudy/Lisa/Week1/todoApp/babel.config.js
+++ ReactNativeStudy/Lisa/Week1/todoApp/babel.config.js
@@ -1,3 +1,29 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@components': './src/components',
+          '@libs': './src/libs',
+          '@screens': './src/screens',
+          '@styles': './src/styles',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
