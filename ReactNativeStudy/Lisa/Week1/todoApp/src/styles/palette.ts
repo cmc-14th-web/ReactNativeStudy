8d4dd9f66f9531f70b30df8b13a153768f897277
ghdtjgus76 <ghdtjgus76@naver.com>
2023-11-27 07:12:50 +0900

    feat: typo, palette 추가

--- ReactNativeStudy/Lisa/Week1/todoApp/src/styles/palette.ts
+++ ReactNativeStudy/Lisa/Week1/todoApp/src/styles/palette.ts
@@ -1,1 +1,11 @@
-export const palette = {};
+export const palette = {
+  orange: '#FF8F50',
+  green: '#57BB73',
+  blue: '#5061FF',
+  purple: '#C750FF',
+  pink: '#FF7474',
+  gray: '#F5F5F5',
+  dark_gray: '#888888',
+  black: '#000000',
+  white: '#FFFFFF',
+};
--- ReactNativeStudy/Lisa/Week1/todoApp/src/styles/typo.ts
+++ ReactNativeStudy/Lisa/Week1/todoApp/src/styles/typo.ts
@@ -1,1 +1,16 @@
-export const typo = {} as const;
+import {css} from 'styled-components';
+
+export const typo = {
+  title: css`
+    font-family: Pretendard;
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 140%;
+  `,
+  body: css`
+    font-family: Pretendard;
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 140%;
+  `,
+} as const;
