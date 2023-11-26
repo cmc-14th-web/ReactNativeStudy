f96df8c58cc54cad93033a5237a80c4fb77fcfef
ghdtjgus76 <ghdtjgus76@naver.com>
2023-11-27 07:00:59 +0900

    feat: 내비게이션 세팅

--- ReactNativeStudy/Lisa/Week1/todoApp/App.tsx
+++ ReactNativeStudy/Lisa/Week1/todoApp/App.tsx
@@ -1,11 +1,23 @@
 import {RecoilRoot} from 'recoil';
 import {ThemeProvider} from 'styled-components';
-import {theme} from './src/styles';
+import {theme} from 'styles';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from 'screens/Home';
+
+// const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <RecoilRoot>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Home" component={Home} />
+          </Tab.Navigator>
+        </NavigationContainer>
         <div />
       </RecoilRoot>
     </ThemeProvider>
