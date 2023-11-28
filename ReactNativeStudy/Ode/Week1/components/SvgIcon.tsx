a266b956e4262117d6c0a2fd03bb8a35d2683084
nimod7890 <nimod1234@g.skku.edu>
2023-11-28 22:24:08 +0900

    refactor: add size props to SvgIcon

--- ReactNativeStudy/Ode/Week1/components/SvgIcon.tsx
+++ ReactNativeStudy/Ode/Week1/components/SvgIcon.tsx
@@ -3,12 +3,15 @@ import Svg, { Path, SvgProps } from "react-native-svg";
 
 type SvgIconProps = {
   label: SvgLabelType;
+  size?: number;
 } & SvgProps;
 
-export default function SvgIcon({ label, ...props }: SvgIconProps) {
+export default function SvgIcon({ label, size, ...props }: SvgIconProps) {
   return (
     <Svg
       {...props}
+      width={size ?? 24}
+      height={size ?? 24}
       viewBox="0 0 24 24"
       // xmlns="http://www.w3.org/2000/svg"
     >
