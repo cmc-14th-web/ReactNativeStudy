0deb2ddd0b61a3988805d182144df659cb96595c
ShinYoung-Kim <lukey0515@naver.com>
2023-11-29 17:22:03 +0900

    feat: bottom navigation 적용

--- ReactNativeStudy/Zero/Week1/Todo/App.tsx
+++ ReactNativeStudy/Zero/Week1/Todo/App.tsx
@@ -7,19 +7,20 @@
 
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
 import Home from './android/app/src/pages/Home';
 import Setting from './android/app/src/pages/Setting';
 
-const Stack = createStackNavigator();
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 function App(): JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Setting" component={Setting} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="홈" component={Home} />
+        <Tab.Screen name="설정" component={Setting} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
