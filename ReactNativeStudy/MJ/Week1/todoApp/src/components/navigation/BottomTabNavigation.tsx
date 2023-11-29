d3bbf61a232ddaad1e1aacec831b2b13e6ccd4fe
minai621 <minai621@naver.com>
2023-11-30 01:54:44 +0900

    style(color): BottomTabNavigation

--- ReactNativeStudy/MJ/Week1/todoApp/src/components/navigation/BottomTabNavigation.tsx
+++ ReactNativeStudy/MJ/Week1/todoApp/src/components/navigation/BottomTabNavigation.tsx
@@ -5,8 +5,10 @@ import {
 import React from 'react';
 import {TouchableOpacity, View} from 'react-native';
 
+import {useRecoilValue} from 'recoil';
 import Home from '../../screens/Home';
 import Setting from '../../screens/Setting';
+import colorState from '../../store/color';
 import palette from '../../styles/palette';
 import {RootStackParamList, RootTabParamList} from '../../types/type';
 import IconFactory from '../IconFactory';
@@ -20,34 +22,64 @@ const BottomTabNavigation = ({
 }: {
   navigation: TabsNavigationProps;
 }) => {
+  const color = useRecoilValue(colorState);
   const handleClick = () => navigation.navigate('AddTodo');
   const screenOptions = {
     headerStyle: {
       backgroundColor: 'transparent',
     },
-    headerTintColor: '#000',
+    headerTintColor: palette[color],
   };
   return (
     <Tab.Navigator screenOptions={screenOptions}>
-      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerTitle: 'Today',
+          tabBarLabel: '홈',
+          tabBarActiveTintColor: palette[color],
+          tabBarIcon: ({focused}) => (
+            <IconFactory
+              icon="Home"
+              fill={focused ? palette[color] : palette.DarkGray}
+            />
+          ),
+        }}
+      />
       <Tab.Screen
         name="AddTodoBtn"
         component={View}
         options={{tabBarButton: () => <AddTodoBtn handleClick={handleClick} />}}
       />
-      <Tab.Screen name="Setting" component={Setting} />
+      <Tab.Screen
+        name="Setting"
+        component={Setting}
+        options={{
+          headerTitle: '설정',
+          tabBarLabel: '설정',
+          tabBarActiveTintColor: palette[color],
+          tabBarIcon: ({focused}) => (
+            <IconFactory
+              icon="Theme"
+              fill={focused ? palette[color] : palette.DarkGray}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
 
 const AddTodoBtn = ({handleClick}: {handleClick: () => void}) => {
+  const color = useRecoilValue(colorState);
   return (
     <TouchableOpacity
       onPress={handleClick}
       style={{
         top: 5,
       }}>
-      <IconFactory icon="Add" width={50} height={50} fill={palette.Black} />
+      <IconFactory icon="Add" width={50} height={50} fill={palette[color]} />
     </TouchableOpacity>
   );
 };
