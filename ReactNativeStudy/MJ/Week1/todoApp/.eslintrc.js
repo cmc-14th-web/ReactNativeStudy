40a6947aa08dacb60f23f0780e6701067a3db377
minai621 <minai621@naver.com>
2023-11-30 00:26:13 +0900

    chore(lint): off the react-in-js-scope rule

--- ReactNativeStudy/MJ/Week1/todoApp/.eslintrc.js
+++ ReactNativeStudy/MJ/Week1/todoApp/.eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.json', '.native.js'],
+      },
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
 };
