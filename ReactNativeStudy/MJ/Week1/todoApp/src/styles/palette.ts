eb1387cf11aafdd5a3c6b3beac00fd60a090d028
minai621 <minai621@naver.com>
2023-11-29 23:39:28 +0900

    feat(style): palette

---
+++ ReactNativeStudy/MJ/Week1/todoApp/src/styles/palette.ts
@@ -0,0 +1,13 @@
+const palette = {
+  Orange: '#FF8f50',
+  Gray: '#F5F5F5',
+  Green: '#57BB73',
+  DarkGray: '#888888',
+  Blue: '#5061FF',
+  Black: '#000000',
+  Purple: '#C750FF',
+  White: '#FFFFFF',
+  Pink: '#FF7474',
+} as const;
+
+export default palette;
