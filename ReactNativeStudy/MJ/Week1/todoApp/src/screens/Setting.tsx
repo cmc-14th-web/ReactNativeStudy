5f7067c1a12a2ebb2123d0457f8638c04f3584eb
minai621 <minai621@naver.com>
2023-11-30 04:13:29 +0900

    feat(setting): implement theme change

--- ReactNativeStudy/MJ/Week1/todoApp/src/screens/Setting.tsx
+++ ReactNativeStudy/MJ/Week1/todoApp/src/screens/Setting.tsx
@@ -1,12 +1,56 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {useSetRecoilState} from 'recoil';
+import colorState from '../store/color';
+import palette, {KeyofPalette} from '../styles/palette';
+
+type themeType = Extract<
+  KeyofPalette,
+  'Orange' | 'Green' | 'Blue' | 'Purple' | 'Pink'
+>;
 
 const Setting = () => {
+  const setColor = useSetRecoilState(colorState);
+  const handlePressColor = (color: themeType) => {
+    setColor(color);
+  };
+
+  const theme: themeType[] = ['Orange', 'Green', 'Blue', 'Purple', 'Pink'];
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.Container}>
+      <Text>색상을 선택해주세요</Text>
+      <View style={styles.PaletteContainer}>
+        {theme.map(color => (
+          <TouchableOpacity
+            key={color}
+            onPress={() => handlePressColor(color)}
+            style={{
+              ...styles.PaletteItem,
+              backgroundColor: palette[color],
+            }}
+          />
+        ))}
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  Container: {
+    padding: 20,
+  },
+  PaletteContainer: {
+    gap: 3,
+    padding: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  PaletteItem: {
+    width: 30,
+    height: 30,
+    borderRadius: 100,
+  },
+});
 
 export default Setting;
