595be8f33d5d2c6548257fe7d9ad8986820ea903
minai621 <minai621@naver.com>
2023-11-29 23:31:56 +0900

    chore: 프로젝트 설치

---
+++ ReactNativeStudy/MJ/Week1/todoApp/src/App.tsx
@@ -0,0 +1,13 @@
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>hello</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
