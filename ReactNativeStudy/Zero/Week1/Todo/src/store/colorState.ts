3294e3b53b3aeb7bf7ba46986c750481b58f387b
ShinYoung-Kim <lukey0515@naver.com>
2023-11-30 21:34:56 +0900

    feat: color atom에 color code 매칭하는 함수 추가

--- ReactNativeStudy/Zero/Week1/Todo/src/store/colorState.ts
+++ ReactNativeStudy/Zero/Week1/Todo/src/store/colorState.ts
@@ -1,4 +1,5 @@
 import {atom, useRecoilState} from 'recoil';
+import {theme} from '../constants';
 
 const colorState = atom({
   key: 'colorState',
@@ -7,10 +8,14 @@ const colorState = atom({
 
 export function useColor() {
   const [color, setColor] = useRecoilState(colorState);
+  const themeColor = theme.color;
   return {
     color,
     changeColor(color: string) {
       setColor(color);
+    },
+    getColorCode() {
+      return themeColor[color as keyof typeof themeColor];
     },
   };
 }
