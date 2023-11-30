1494df2371cf1b16b010731b323e8a80f7ace3bf
ShinYoung-Kim <lukey0515@naver.com>
2023-11-30 20:24:10 +0900

    feat: setting page에 color state 적용

--- ReactNativeStudy/Zero/Week1/Todo/src/pages/Setting.tsx
+++ ReactNativeStudy/Zero/Week1/Todo/src/pages/Setting.tsx
@@ -1,11 +1,54 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
+import { theme } from '../constants';
+import Icon from '../components/Icon';
+import React from 'react';
+import { useColor } from '../store/colorState';
+
+const colors: (keyof typeof theme.color)[] = ['Orange', 'Green', 'Blue', 'Purple', 'Pink'];
 
 function Setting() {
+    const selectedColor = useColor().color;
+    const handleChangeSelectedColor = useColor().changeColor;
+
     return (
-        <View>
-            <Text>Setting</Text>
+        <View style={style.container}>
+            <Text style={style.text}>색상을 선택해주세요.</Text>
+            <View style={style.colorWrapper}>
+                {colors.map((color) => (
+                    color === selectedColor ?
+                        <Icon width="30" height="30" type="check" fill={theme.color[color]} key={color} />
+                        : <View style={{
+                            ...style.selectedColor,
+                            backgroundColor: theme.color[color],
+                        }}
+                            key={color}
+                            onTouchEnd={() => handleChangeSelectedColor(color)}
+                        />
+                ))}
+            </View>
         </View>
-    )
+    );
 }
+
+const style = StyleSheet.create({
+    container: {
+        paddingHorizontal: 32,
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: '400',
+        color: theme.color.Black,
+    },
+    colorWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    selectedColor: {
+        width: 24,
+        height: 24,
+        borderRadius: 100,
+    }
+});
 
 export default Setting;
