e264f8931fb6393c9e386b709fcb6453d041029a
ShinYoung-Kim <lukey0515@naver.com>
2023-11-30 23:47:04 +0900

    feat: stack navigation option 분리

--- ReactNativeStudy/Zero/Week1/Todo/src/navigation/StackNavigation.tsx
+++ ReactNativeStudy/Zero/Week1/Todo/src/navigation/StackNavigation.tsx
@@ -1,20 +1,21 @@
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react'
+
 import Routing from '../utility/Routing';
 import NewTask from '../pages/NewTask';
 
 const Stack = createStackNavigator();
+const stackNavigationOptions = {
+    headerShown: false,
+};
+
 function StackNavigation() {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Routing" component={Routing} options={{
-                headerShown: false,
-            }} />
-            <Stack.Screen name="할 일을 추가해주세요3" component={NewTask} options={{
-                headerShown: false,
-            }} />
+            <Stack.Screen name="Routing" component={Routing} options={stackNavigationOptions} />
+            <Stack.Screen name="할 일을 추가해주세요3" component={NewTask} options={stackNavigationOptions} />
         </Stack.Navigator>
-    )
+    );
 }
 
 export default StackNavigation;
