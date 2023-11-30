7ef7f4533241e49668b390ae793e9f91ab3baaf1
ShinYoung-Kim <lukey0515@naver.com>
2023-11-30 17:06:35 +0900

    feat: 색상 상수 생성

---
+++ ReactNativeStudy/Zero/Week1/Todo/src/constants.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  color: {
+    Orange: '#FF8F50',
+    DarkGray: '#F5F5F5',
+    Green: '#57BB73',
+    Gray: '#888888',
+    Blue: '#5061FF',
+    Black: '#000000',
+    Purple: '#C750FF',
+    White: '#FFFFFF',
+    Pink: '#FF7474',
+  },
+};
