3f97b38a902c4d4685a346d6a5903b37abcf30a1
ShinYoung-Kim <lukey0515@naver.com>
2023-11-30 19:35:22 +0900

    feat: todo 추가 함수 연결

--- ReactNativeStudy/Zero/Week1/Todo/src/pages/NewTask.tsx
+++ ReactNativeStudy/Zero/Week1/Todo/src/pages/NewTask.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { theme } from '../constants';
+import { useTodos } from '../store/todosState';
 
 function NewTask() {
     const [value, setValue] = useState('');
+    const addTask = useTodos().addTodos;
+    const handleSubmit = () => {
+        addTask(value);
+    };
 
     return (
         <View>
             <Text>할 일을 추가해주세요!</Text>
-            <Button title="완료" />
+            <Button title="완료" onPress={handleSubmit} />
             <View style={style.container}>
                 <TextInput style={style.textInput} placeholder="할 일을 입력해주세요" onChangeText={setValue} value={value} />
             </View>
