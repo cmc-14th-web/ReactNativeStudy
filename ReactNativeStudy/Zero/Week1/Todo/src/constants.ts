37abcac9b939afc07d492f95d16c6900c9afa19e
ShinYoung-Kim <lukey0515@naver.com>
2023-11-30 22:24:01 +0900

    feat: them color 일부 변경

--- ReactNativeStudy/Zero/Week1/Todo/src/constants.ts
+++ ReactNativeStudy/Zero/Week1/Todo/src/constants.ts
@@ -1,9 +1,9 @@
 export const theme = {
   color: {
     Orange: '#FF8F50',
-    DarkGray: '#F5F5F5',
+    Gray: '#F5F5F5',
     Green: '#57BB73',
-    Gray: '#888888',
+    DarkGray: '#888888',
     Blue: '#5061FF',
     Black: '#000000',
     Purple: '#C750FF',
