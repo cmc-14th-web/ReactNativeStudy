a5a8f178d38a83b1528243e4270e6d5ce5b2349d
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-12-01 07:52:17 +0900

    feat: 초기 테마 및 색상 팔레트 설정 추가

---
+++ ReactNativeStudy/Sarah/Week1/ToDoList/src/theme/theme.tsx
@@ -0,0 +1,22 @@
+const palette: Record<string, string> = {
+  orange: '#FF8F50',
+  green: '#57BB73',
+  blue: '#5061FF',
+  purple: '#C750FF',
+  pink: '#FF7474',
+};
+
+const colors = {
+  gray: '#F5F5F5',
+  darkGray: '#888888',
+  black: '#000000',
+  white: '#FFFFFF',
+};
+
+const theme = {
+  palette,
+  colors,
+  selectedTheme: '',
+};
+
+export default theme;
