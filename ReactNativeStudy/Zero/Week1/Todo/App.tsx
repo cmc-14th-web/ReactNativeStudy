deedc428a54da735cac8b0a597097bf495ba9f7d
ShinYoung-Kim <lukey0515@naver.com>
2023-11-30 21:34:16 +0900

    feat: 페이지 header 안 보이게 설정

--- ReactNativeStudy/Zero/Week1/Todo/App.tsx
+++ ReactNativeStudy/Zero/Week1/Todo/App.tsx
@@ -21,9 +21,18 @@ function App(): JSX.Element {
     <RecoilRoot>
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="홈" component={Home} />
-          <Tab.Screen name="할 일을 추가해주세요" component={NewTask} />
-          <Tab.Screen name="설정" component={Setting} />
+          <Tab.Screen name="홈" component={Home}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Tab.Screen name="할 일을 추가해주세요" component={NewTask} options={{
+            tabBarStyle: { display: 'none' },
+            headerShown: false,
+          }} />
+          <Tab.Screen name="설정" component={Setting} options={{
+            headerShown: false,
+          }} />
         </Tab.Navigator>
       </NavigationContainer>
     </RecoilRoot>
