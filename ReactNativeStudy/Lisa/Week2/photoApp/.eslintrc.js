3fe2b68310e2e8458c930782d95103e64fdab993
ghdtjgus76 <ghdtjgus76@naver.com>
2023-12-04 08:12:56 +0900

    feat: eslint 설정

--- ReactNativeStudy/Lisa/Week2/photoApp/.eslintrc.js
+++ ReactNativeStudy/Lisa/Week2/photoApp/.eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/no-unstable-nested-components': 'off',
+    'react-native/no-inline-styles': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+  },
 };
