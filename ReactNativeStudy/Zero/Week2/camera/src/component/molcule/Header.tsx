d4c19e423fd8c03eae4855755011d59b14f217a7
ShinYoung-Kim <lukey0515@naver.com>
2023-12-06 02:25:25 +0900

    feat: header 컴포넌트 생성

---
+++ ReactNativeStudy/Zero/Week2/camera/src/component/molcule/Header.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet, Text, View } from "react-native";
+import { COLOR, GRADIENT } from "../../constants/color";
+import { LinearGradientText } from "react-native-linear-gradient-text";
+
+interface HeaderProps {
+    title: string;
+}
+
+function Header({ title }: HeaderProps) {
+    return (
+        <View style={styles.container}>
+            <LinearGradientText
+                colors={[COLOR.Pink, COLOR.Purple]}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 0 }}
+                text={title}
+                textStyle={styles.title}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        paddingHorizontal: 18,
+        paddingVertical: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: '600',
+    },
+});
+
+export default Header;
