98f2459a7a2cb265f5503d3e3316ede2b4831868
coddingyun <igy2840@gmail.com>
2023-12-05 15:05:16 +0900

    feat: splash screen 제작

--- ReactNativeStudy/Eurali/Week2/cameraApp/src/App.tsx
+++ ReactNativeStudy/Eurali/Week2/cameraApp/src/App.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import {Text} from 'react-native';
 
 function App(): JSX.Element {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 2000); //스플래시 활성화 시간
+  });
   return <Text>haha</Text>;
 }
 
--- ReactNativeStudy/Eurali/Week2/cameraApp/ios/cameraApp/AppDelegate.mm
+++ ReactNativeStudy/Eurali/Week2/cameraApp/ios/cameraApp/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import "RNSplashScreen.h"
 
 #import <React/RCTBundleURLProvider.h>
 
@@ -11,7 +12,10 @@
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  [super application:application didFinishLaunchingWithOptions:launchOptions];
+  [RNSplashScreen show];  // 추가
+
+  return YES; // 수정
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
