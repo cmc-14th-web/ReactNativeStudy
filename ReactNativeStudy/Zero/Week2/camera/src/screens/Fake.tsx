d21f9aa71387901ce7c6b829e524891d296135fb
ShinYoung-Kim <lukey0515@naver.com>
2023-12-05 23:47:43 +0900

    feat: navigate할 스크린 생성

---
+++ ReactNativeStudy/Zero/Week2/camera/src/screens/Fake.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function Fake() {
+    return (
+        <View>
+            <Text>Fake</Text>
+        </View>
+    );
+}
+
+export default Fake;
---
+++ ReactNativeStudy/Zero/Week2/camera/src/screens/Setting.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function Setting() {
+    return (
+        <View>
+            <Text>Setting</Text>
+        </View>
+    );
+}
+
+export default Setting;
