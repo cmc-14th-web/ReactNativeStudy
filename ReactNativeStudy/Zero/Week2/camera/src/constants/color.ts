5662321b167523b464a80df6438f8b08efc303c8
ShinYoung-Kim <lukey0515@naver.com>
2023-12-05 20:24:10 +0900

    chore: color, gradient 상수 추가

---
+++ ReactNativeStudy/Zero/Week2/camera/src/constants/color.ts
@@ -0,0 +1,13 @@
+export const COLOR = {
+  Purple: '#A45CFF',
+  Pink: '#E33AFF',
+  Black: '#000000',
+  White: '#FFFFFF',
+  Gray400: '#888888',
+  Gray600: '#282828',
+  Gray900: '#121212',
+};
+
+export const GRADIENT = {
+  Gradient100: `linear-gradient(90deg, ${COLOR.Pink} 0%, ${COLOR.Purple} 100%)`,
+};
