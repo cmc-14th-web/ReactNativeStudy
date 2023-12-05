0d8702cf36534a0514dfde8363dd3fcec94934b1
ShinYoung-Kim <lukey0515@naver.com>
2023-12-06 03:01:50 +0900

    feat: bottom tab navigation에 gradient 적용

--- ReactNativeStudy/Zero/Week2/camera/src/navigation/BottomTabNavigation.tsx
+++ ReactNativeStudy/Zero/Week2/camera/src/navigation/BottomTabNavigation.tsx
@@ -2,15 +2,54 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Fake from "../screens/Fake";
 import Setting from "../screens/Setting";
+import { Icon } from "../component/atom/Icon";
+import { COLOR, GRADIENT } from "../constants/color";
+import CustomTabBarButton from "../component/molcule/CustomTabBarButton";
+import { View } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
 function BottomTabNavigation() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Add" component={Fake} />
-            <Tab.Screen name="Settings" component={Setting} />
+        <Tab.Navigator
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    backgroundColor: COLOR.Gray900,
+                    height: 60,
+                    borderTopWidth: 0.2,
+                },
+                headerShown: false,
+            }}
+        >
+            <Tab.Screen name="Home" component={Home}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <CustomTabBarButton name="Home" focused={focused} title='홈' />
+                    ),
+                }}
+            />
+            <Tab.Screen name="Add" component={Fake}
+                options={{
+                    tabBarIcon: () => (
+                        <View
+                            style={{
+                                position: 'absolute',
+                                bottom: 30,
+                            }}
+                        >
+                            <Icon name="Plus" size={60} fill={GRADIENT.Gradient100} />
+                        </View>
+                    ),
+                }}
+            />
+            <Tab.Screen name="Settings" component={Setting}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <CustomTabBarButton name="Theme" focused={focused} title='설정' />
+                    ),
+                }}
+            />
         </Tab.Navigator>
     );
 }
