38048b2d85cb383cc33916dca09f587953451e83
ghdtjgus76 <ghdtjgus76@naver.com>
2023-12-05 12:11:42 +0900

    feat: 스플래시 화면 추가

--- ReactNativeStudy/Lisa/Week2/photoApp/App.tsx
+++ ReactNativeStudy/Lisa/Week2/photoApp/App.tsx
@@ -1,11 +1,18 @@
 import Navigation from 'navigators/Navigation';
 import {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import {
   checkCameraPermission,
   checkExternalStoragePermission,
 } from 'utils/checkPermission';
 
 function App(): JSX.Element {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000);
+  });
+
   useEffect(() => {
     checkCameraPermission();
     checkExternalStoragePermission();
