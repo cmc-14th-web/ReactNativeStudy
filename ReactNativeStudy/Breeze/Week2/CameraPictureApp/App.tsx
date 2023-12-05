4f93e79f8dfb14e7fd5e37f25867c1a0565ccff3
suminhan123 <suminh324@gmail.com>
2023-12-05 18:37:04 +0900

    feat: 프로젝트 생성

---
+++ ReactNativeStudy/Breeze/Week2/CameraPictureApp/App.tsx
@@ -0,0 +1,30 @@
+import {Text, View, StyleSheet} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>메인페이지</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
