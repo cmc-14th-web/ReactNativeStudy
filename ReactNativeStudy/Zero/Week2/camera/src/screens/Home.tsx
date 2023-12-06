26fcc72999e7ace35f2a83ddbafce87558023d19
ShinYoung-Kim <lukey0515@naver.com>
2023-12-07 03:23:19 +0900

    feat: home에서 이미지 개수에 따른 렌더링 제어 추가

--- ReactNativeStudy/Zero/Week2/camera/src/screens/Home.tsx
+++ ReactNativeStudy/Zero/Week2/camera/src/screens/Home.tsx
@@ -1,12 +1,36 @@
 import { Text } from 'react-native';
 import BackgroundContainer from '../component/atom/BackgroundContainer';
 import Header from '../component/molcule/Header';
+import { storage } from '../utils/storage';
+import { useEffect, useState } from 'react';
+import EmptyGalleryTemplates from '../component/templates/EmptyGalleryTemplate';
+import { Image } from '../type/image';
+import ImageGalleryTemplate from '../component/templates/ImageGalleryTemplate';
+import { useImages } from '../hooks/useImages';
+import { useIsFocused } from '@react-navigation/native';
 
 function Home() {
+    const { getImages } = useImages();
+    const [images, setImages] = useState<Image[]>([]);
+    const isFocused = useIsFocused();
+
+    useEffect(() => {
+        const images = getImages();
+        setImages(images);
+    }, [isFocused]);
+
+    const renderImages = () => {
+        if (images.length > 0) {
+            return <ImageGalleryTemplate images={images} />;
+        }
+
+        return <EmptyGalleryTemplates />;
+    };
+
     return (
         <BackgroundContainer>
             <Header title="CMC님의 사진첩" />
-            <Text>Home</Text>
+            {renderImages()}
         </BackgroundContainer>
     );
 }
