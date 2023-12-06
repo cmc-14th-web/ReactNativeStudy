fa71ab89694f54654171b81d1323d5f531edd209
ShinYoung-Kim <lukey0515@naver.com>
2023-12-07 05:19:56 +0900

    feat: header에 backbutton 추가

--- ReactNativeStudy/Zero/Week2/camera/src/component/molcule/Header.tsx
+++ ReactNativeStudy/Zero/Week2/camera/src/component/molcule/Header.tsx
@@ -1,21 +1,36 @@
 import { StyleSheet, Text, View } from "react-native";
 import { COLOR, GRADIENT } from "../../constants/color";
 import { LinearGradientText } from "react-native-linear-gradient-text";
+import IconButton from "./IconButton";
+import { useNavigation } from "@react-navigation/native";
 
 interface HeaderProps {
     title: string;
+    isBackButton?: boolean;
 }
 
-function Header({ title }: HeaderProps) {
+function Header({ title, isBackButton }: HeaderProps) {
+    const navigation = useNavigation();
+    const handleBackButton = () => {
+        navigation.goBack();
+    }
+
     return (
-        <View style={styles.container}>
+        <View style={{
+            ...styles.container,
+            justifyContent: isBackButton ? 'space-between' : 'flex-start',
+        }}>
+            {isBackButton && <IconButton name="ArrowBack" size={24} fill={GRADIENT.Gradient100} onPress={handleBackButton} />}
             <LinearGradientText
                 colors={[COLOR.Pink, COLOR.Purple]}
                 start={{ x: 0, y: 0 }}
                 end={{ x: 1, y: 0 }}
                 text={title}
-                textStyle={styles.title}
+                textStyle={{
+                    ...styles.title,
+                }}
             />
+            {isBackButton && <View style={{ width: 24 }} />}
         </View>
     )
 }
@@ -28,6 +43,7 @@ const styles = StyleSheet.create({
         top: 0,
         left: 0,
         width: '100%',
+        flexDirection: 'row',
     },
     title: {
         fontSize: 24,
