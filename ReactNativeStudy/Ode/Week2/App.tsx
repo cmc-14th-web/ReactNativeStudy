e786b92d8da293215c0fc6c100f821170d7982e6
nimod7890 <nimod1234@g.skku.edu>
2023-12-08 00:26:32 +0900

    feat: root stack navigator

--- ReactNativeStudy/Ode/Week2/App.tsx
+++ ReactNativeStudy/Ode/Week2/App.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
+import React, {useEffect} from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {NavigationContainer} from '@react-navigation/native';
+import {SafeAreaView, Text, View} from 'react-native';
+import {requestPermissions} from './src/utils/requestPermissions';
+import RecoilSetting from './src/libraries/recoil';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import RootNavigator from './src/navigation/RootNavigator';
 
-function App(): JSX.Element {
-  return <SafeAreaView />;
+export default function App(): JSX.Element {
+  useEffect(() => {
+    requestPermissions();
+  }, []);
+
+  return (
+    <GestureHandlerRootView>
+      <SafeAreaProvider>
+        <RecoilSetting>
+          <NavigationContainer>
+            <RootNavigator />
+          </NavigationContainer>
+        </RecoilSetting>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
 }
 
-export default App;
+export const Detail = () => (
+  <SafeAreaView>
+    <View>
+      <Text>hi</Text>
+    </View>
+  </SafeAreaView>
+);
