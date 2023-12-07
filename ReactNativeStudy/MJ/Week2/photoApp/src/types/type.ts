be172d2d01506bce486509f8dbefc58ee0b0131a
minai621 <minai621@naver.com>
2023-12-07 23:22:30 +0900

    feat: 스택 네비게이션 설정

---
+++ ReactNativeStudy/MJ/Week2/photoApp/src/types/type.ts
@@ -0,0 +1,11 @@
+export type RootStackParamList = {
+  Home: undefined;
+  Gallery: undefined;
+  BottomTabNavigation: undefined;
+};
+
+export type RootTabParamList = {
+  Home: undefined;
+  CameraDraw: undefined;
+  Setting: undefined;
+};
