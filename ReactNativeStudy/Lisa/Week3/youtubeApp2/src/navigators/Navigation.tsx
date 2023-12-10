27e9c78739c3f8df12c6be6eeea84318218edd39
ghdtjgus76 <ghdtjgus76@naver.com>
2023-12-10 17:33:05 +0900

    feat: 전역 theme 설정

--- ReactNativeStudy/Lisa/Week3/youtubeApp2/src/navigators/Navigation.tsx
+++ ReactNativeStudy/Lisa/Week3/youtubeApp2/src/navigators/Navigation.tsx
@@ -1,9 +1,19 @@
-import {NavigationContainer} from '@react-navigation/native';
+import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
+
 import StackNavigator from './StackNavigator';
+import {theme} from 'styles/theme';
+
+const globalTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: theme.palette.gray_900,
+  },
+};
 
 const Navigation = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={globalTheme}>
       <StackNavigator />
     </NavigationContainer>
   );
