baaef35e9b531e48e520c8165e06b73e15cceb47
ShinYoung-Kim <lukey0515@naver.com>
2023-12-11 17:38:21 +0900

    feat: home, setting 스크린 생성

---
+++ ReactNativeStudy/Zero/Week3/youtube/src/screen/Home.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+function Home() {
+    return (
+        <View>
+            <Text>
+                home입니다.
+            </Text>
+        </View>
+    )
+}
+
+export default Home;
