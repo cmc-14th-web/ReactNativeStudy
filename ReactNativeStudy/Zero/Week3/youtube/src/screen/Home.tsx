183763d8d6537cd9a2a7af621aa28e6654e74795
ShinYoung-Kim <lukey0515@naver.com>
2023-12-12 03:52:31 +0900

    feat: 유튜브 인기 동영상 조회 연결

--- ReactNativeStudy/Zero/Week3/youtube/src/screen/Home.tsx
+++ ReactNativeStudy/Zero/Week3/youtube/src/screen/Home.tsx
@@ -1,13 +1,34 @@
-import { Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import VideoItem from "../component/VideoItem";
+import { COLOR } from "../constant/color";
+import { useGetMostPopularVideoList } from "../api/mostPopularVideo";
+import { videoInfo } from "../type/videoInfo";
 
 function Home() {
+    const { mostPopularVideoList, isLoading, error } = useGetMostPopularVideoList();
     return (
-        <View>
-            <Text>
-                home입니다.
+        <View style={{
+            backgroundColor: COLOR.Gray900,
+            height: '100%',
+        }}>
+            <Text style={style.listTitle}>
+                인기 동영상
             </Text>
+            {mostPopularVideoList?.map((videoInfo: videoInfo) => {
+                return <VideoItem key={videoInfo.id} videoInfo={videoInfo} />
+            })}
         </View>
     )
 }
+
+const style = StyleSheet.create({
+    listTitle: {
+        color: COLOR.Red,
+        fontSize: 18,
+        fontWeight: '600',
+        paddingHorizontal: 18,
+        paddingVertical: 10,
+    }
+});
 
 export default Home;
