d91afb170cf7f5081c223866db624aaacb125ef4
ShinYoung-Kim <lukey0515@naver.com>
2023-12-11 22:51:24 +0900

    feat: const 추가

---
+++ ReactNativeStudy/Zero/Week3/youtube/src/constant/color.ts
@@ -0,0 +1,12 @@
+export const COLOR = {
+  Red: '#EB3323',
+  Black: '#000000',
+  White: '#FFFFFF',
+  DarkGray: '#888888',
+  Gray100: '#F5F5F5',
+  Gray600: '#8B8B8B',
+  Gray800: '#272727',
+  Gray900: '#121212',
+};
+
+export type ColorType = keyof typeof COLOR;
