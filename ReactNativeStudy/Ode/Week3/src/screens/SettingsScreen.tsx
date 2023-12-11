d7a3b402adc26d6956495c1b0466d54799e9887f
nimod7890 <nimod1234@g.skku.edu>
2023-12-12 02:07:25 +0900

    feat: setting page

---
+++ ReactNativeStudy/Ode/Week3/src/screens/SettingsScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '../components/common/Container';
+import {Text} from 'react-native';
+import Colors from '../styles/colors';
+
+export default function SettingsScreen() {
+  return (
+    <Container>
+      <Text style={{color: Colors.White}}>setting</Text>
+    </Container>
+  );
+}
