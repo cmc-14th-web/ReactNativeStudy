1db74547913fc4146e0de66112c26ad1408e6341
ShinYoung-Kim <lukey0515@naver.com>
2023-12-11 22:51:44 +0900

    feat: bottom tab navigation에 icon 연결

--- ReactNativeStudy/Zero/Week3/youtube/src/navigation/BottomTabNavigation.tsx
+++ ReactNativeStudy/Zero/Week3/youtube/src/navigation/BottomTabNavigation.tsx
@@ -1,14 +1,28 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screen/Home";
 import Setting from "../screen/Setting";
+import { Icon } from "../component/Icon";
+import { COLOR } from "../constant/color";
 
 const Tab = createBottomTabNavigator();
 
 function BottomTabNavigation() {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Settings" component={Setting} />
+            <Tab.Screen name="Home" component={Home}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <Icon id="Home" width={28} height={28} fill={focused ? COLOR.Red : COLOR.DarkGray} />
+                    )
+                }}
+            />
+            <Tab.Screen name="Settings" component={Setting}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <Icon id="Theme" width={28} height={28} fill={focused ? COLOR.Red : COLOR.DarkGray} />
+                    )
+                }}
+            />
         </Tab.Navigator>
     )
 }
