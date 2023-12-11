040c2b28b624e2f6cd9937ce07c9ca4c27d3c0a7
ShinYoung-Kim <lukey0515@naver.com>
2023-12-11 22:51:34 +0900

    feat: 아이콘 컴포넌트 생성

---
+++ ReactNativeStudy/Zero/Week3/youtube/src/component/Icon.tsx
@@ -0,0 +1,17 @@
+import { Svg, Path } from "react-native-svg";
+import { ICON, IconType } from "../constant/icon";
+
+interface IconProps {
+    id: IconType;
+    fill?: string;
+    width?: number;
+    height?: number;
+}
+
+export function Icon({ id, fill, width, height }: IconProps) {
+    return (
+        <Svg width={width} height={height} viewBox="0 0 24 24">
+            <Path d={ICON[id]} fill={fill} />
+        </Svg>
+    )
+}
