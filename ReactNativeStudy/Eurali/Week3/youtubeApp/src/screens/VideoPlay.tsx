215be7515a7a1d7a182301106d22929062d8c6a2
coddingyun <igy2840@gmail.com>
2023-12-13 17:57:59 +0900

    refactor: YoutubeIframe 필요없는 속성 제거

--- ReactNativeStudy/Eurali/Week3/youtubeApp/src/screens/VideoPlay.tsx
+++ ReactNativeStudy/Eurali/Week3/youtubeApp/src/screens/VideoPlay.tsx
@@ -1,6 +1,5 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import React, {useCallback, useState} from 'react';
-import {Alert} from 'react-native';
+import React from 'react';
 import YoutubeIframe from 'react-native-youtube-iframe';
 import {RootStackParamList} from '../types/navigators';
 import Container from '../layout/Container';
@@ -9,23 +8,9 @@ import VideoInfo from '../components/VideoInfo';
 type VideoPlayProps = NativeStackScreenProps<RootStackParamList, 'VideoPlay'>;
 
 const VideoPlay = ({route}: VideoPlayProps) => {
-  const [playing, setPlaying] = useState(false);
-
-  const onStateChange = useCallback((state: string) => {
-    if (state === 'ended') {
-      setPlaying(false);
-      Alert.alert('video has finished playing!');
-    }
-  }, []);
-
   return (
     <Container>
-      <YoutubeIframe
-        height={230}
-        play={playing}
-        videoId={route.params.videoId}
-        onChangeState={onStateChange}
-      />
+      <YoutubeIframe height={230} videoId={route.params.videoId} />
       <VideoInfo videoInfo={route.params.videoInfo} />
     </Container>
   );
