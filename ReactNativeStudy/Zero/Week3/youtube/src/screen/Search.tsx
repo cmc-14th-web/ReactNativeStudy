9b18923bac3152648679d184c26757fc00c94399
ShinYoung-Kim <lukey0515@naver.com>
2023-12-13 14:04:14 +0900

    feat: 검색 결과에 따라 search screen 렌더링 적용

--- ReactNativeStudy/Zero/Week3/youtube/src/screen/Search.tsx
+++ ReactNativeStudy/Zero/Week3/youtube/src/screen/Search.tsx
@@ -3,12 +3,24 @@ import SearchHeader from "../component/SearchHeader";
 import { useGetSearchResults } from "../api/search";
 import SearchResultList from "../component/SearchResultList";
 import { COLOR } from "../constant/color";
+import { useSearchStore } from "../store/searchStore";
+import EmptySearchResult from "../component/EmptySearchResult";
 
 function Search() {
+    const isSearched = useSearchStore(state => state.isSearched);
+
+    const renderSearchResult = () => {
+        if (isSearched) {
+            return <SearchResultList />
+        }
+
+        return <EmptySearchResult />
+    }
+
     return (
         <View style={style.container}>
             <SearchHeader />
-            <SearchResultList />
+            {renderSearchResult()}
         </View>
     );
 }
