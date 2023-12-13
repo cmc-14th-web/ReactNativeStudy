64f7c5efeca3e4d800ca145f3dce92df1c9e6d08
ShinYoung-Kim <lukey0515@naver.com>
2023-12-13 14:03:34 +0900

    design: 빈 검색 화면 스타일링

--- ReactNativeStudy/Zero/Week3/youtube/src/component/EmptySearchResult.tsx
+++ ReactNativeStudy/Zero/Week3/youtube/src/component/EmptySearchResult.tsx
@@ -1,11 +1,23 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { COLOR } from "../constant/color";
 
 function EmptySearchResult() {
     return (
-        <View>
-            <Text>검색어를 입력하세요.</Text>
+        <View style={style.container}>
+            <Text style={style.text}>검색어를 입력하세요.</Text>
         </View>
     )
 }
+
+const style = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+    },
+    text: {
+        color: COLOR.Gray100,
+    }
+})
 
 export default EmptySearchResult;
