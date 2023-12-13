59c19c00ccfadf4f60be845ac18876f461f814b1
ShinYoung-Kim <lukey0515@naver.com>
2023-12-14 01:25:57 +0900

    feat: 에러 컴포넌트 생성

---
+++ ReactNativeStudy/Zero/Week3/youtube/src/component/Error.tsx
@@ -0,0 +1,25 @@
+import { View, Text, StyleSheet } from "react-native";
+import { COLOR } from "../constant/color";
+
+function Error() {
+    return (
+        <View style={style.container}>
+            <Text style={style.text}>에러가 발생하였습니다.{`\n`}잠시 후 다시 시도해주세요.</Text>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+    },
+    text: {
+        fontSize: 20,
+        color: COLOR.Gray100,
+        textAlign: 'center',
+    }
+});
+
+export default Error;
