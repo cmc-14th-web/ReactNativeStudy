741e8ede21cbff1b9bb9eab04d9cbe02ebff2dd0
minai621 <minai621@naver.com>
2023-12-13 23:47:20 +0900

    feat: wrap root QueryClientProvider

--- ReactNativeStudy/MJ/Week3/youtubeApp/src/App.tsx
+++ ReactNativeStudy/MJ/Week3/youtubeApp/src/App.tsx
@@ -1,16 +1,21 @@
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import React from 'react';
 import 'react-native-gesture-handler';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {RecoilRoot} from 'recoil';
 import Navigator from './components/naviagator/Navigator';
 
+const queryClinet = new QueryClient();
+
 const App = () => {
   return (
     <>
       <RecoilRoot>
-        <SafeAreaProvider>
-          <Navigator />
-        </SafeAreaProvider>
+        <QueryClientProvider client={queryClinet}>
+          <SafeAreaProvider>
+            <Navigator />
+          </SafeAreaProvider>
+        </QueryClientProvider>
       </RecoilRoot>
     </>
   );
