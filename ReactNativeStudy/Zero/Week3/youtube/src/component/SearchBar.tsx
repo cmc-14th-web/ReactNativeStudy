bb131cfc23694204c3ff038dc7a8a46c0f231d52
ShinYoung-Kim <lukey0515@naver.com>
2023-12-13 13:41:02 +0900

    feat: search bar 컴포넌트 생성

---
+++ ReactNativeStudy/Zero/Week3/youtube/src/component/SearchBar.tsx
@@ -0,0 +1,47 @@
+import { TextInput, StyleSheet, View } from "react-native";
+import { COLOR } from "../constant/color";
+import { useState } from "react";
+import IconButton from "./IconButton";
+
+function SearchBar() {
+    const [searchText, setSearchText] = useState('');
+    const handleChangeText = (text: string) => {
+        setSearchText(text);
+    }
+
+    const clearSearchText = () => {
+        setSearchText('');
+    }
+
+    return (
+        <View style={style.container}>
+            <TextInput
+                placeholder="Youtube 검색"
+                style={style.input}
+                placeholderTextColor={COLOR.Gray600}
+                inputMode="search"
+                value={searchText}
+                onChangeText={handleChangeText}
+            />
+            <IconButton id="Close" width={22} height={22} fill={COLOR.Gray600} handlePress={clearSearchText} />
+        </View>
+    );
+}
+
+const style = StyleSheet.create({
+    container: {
+        backgroundColor: COLOR.Gray800,
+        paddingHorizontal: 14,
+        paddingVertical: 6,
+        borderRadius: 20,
+        flexDirection: 'row',
+    },
+    input: {
+        color: COLOR.Gray600,
+        fontSize: 14,
+        fontWeight: '400',
+        flex: 1,
+    }
+})
+
+export default SearchBar;
