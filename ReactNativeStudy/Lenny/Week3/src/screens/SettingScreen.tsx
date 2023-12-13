9078223ca1343a7da67ef63f243f138addadb78c
gs0428 <lider28@naver.com>
2023-12-14 01:34:00 +0900

    feat: 설정 페이지 구현

--- ReactNativeStudy/Lenny/Week3/src/screens/SettingScreen.tsx
+++ ReactNativeStudy/Lenny/Week3/src/screens/SettingScreen.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import Container from '../components/Container';
 
 export default function SettingScreen() {
-  return <Text>SettingScreen</Text>;
+  return (
+    <Container>
+      <View style={styles.wrapStyle}>
+        <Text style={styles.fontStyle}>준비 중 입니다.</Text>
+      </View>
+    </Container>
+  );
 }
+
+const styles = StyleSheet.create({
+  wrapStyle: {
+    flex: 1,
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  fontStyle: {
+    color: 'white',
+    fontSize: 24,
+  },
+});
