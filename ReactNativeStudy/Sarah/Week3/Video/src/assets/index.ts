dc0eb1c6aaf2c8c1253193855a8a9ace78c81ba9
soo yeon <80268199+sypaik-dev@users.noreply.github.com>
2023-12-14 22:06:54 +0900

    chore: assets svg 파일 추가

---
+++ ReactNativeStudy/Sarah/Week3/Video/src/assets/index.ts
@@ -0,0 +1,6 @@
+export {default as logo} from './logo.svg';
+export {default as home} from './home.svg';
+export {default as back} from './back.svg';
+export {default as setting} from './setting.svg';
+export {default as search} from './search.svg';
+export {default as delete} from './delete.svg';
