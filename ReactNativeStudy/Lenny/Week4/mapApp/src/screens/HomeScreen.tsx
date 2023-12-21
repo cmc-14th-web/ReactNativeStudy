9e9df6732b9fc6cbb604dc2e362ac2797378f27e
gs0428 <lider28@naver.com>
2023-12-21 18:06:36 +0900

    feat: App Web 지도 연결 (message handling X)

--- ReactNativeStudy/Lenny/Week4/mapApp/src/screens/HomeScreen.tsx
+++ ReactNativeStudy/Lenny/Week4/mapApp/src/screens/HomeScreen.tsx
@@ -1,6 +1,32 @@
-import React from 'react';
-import {Text} from 'react-native';
+import {WEB_URI} from '@env';
+import React, {useRef} from 'react';
+import {Dimensions, SafeAreaView, StyleSheet} from 'react-native';
+import WebView from 'react-native-webview';
+
+const {width, height} = Dimensions.get('screen');
 
 export default function HomeScreen() {
-  return <Text>HomeScreen</Text>;
+  const currentRef = useRef<WebView>(null);
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView
+        ref={currentRef}
+        style={styles.webview}
+        source={{uri: `${WEB_URI}`}}
+      />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  webview: {
+    flex: 1,
+    width: width,
+    height: height,
+  },
+});
