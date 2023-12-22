49af7da7c6ad2cfcbe3a62bad82e146d098e5284
gs0428 <lider28@naver.com>
2023-12-22 23:46:38 +0900

    chore: Web script 위치 수정

--- ReactNativeStudy/Lenny/Week4/mapWeb/src/App.tsx
+++ ReactNativeStudy/Lenny/Week4/mapWeb/src/App.tsx
@@ -1,10 +1,6 @@
 import { useEffect } from "react";
 import "./App.css";
 
-const script = document.createElement("script");
-script.src = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${import.meta.env.VITE_NAVER_MAPS_CLIENT_ID}`;
-document.head.appendChild(script);
-
 export default function App() {
   useEffect(() => {
     const center: naver.maps.LatLng = new naver.maps.LatLng(37.5666103, 126.9783882);
