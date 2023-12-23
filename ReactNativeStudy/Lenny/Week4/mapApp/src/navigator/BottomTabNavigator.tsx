50fe8d97ac2aef73e5f83cadc900d516e2fe06aa
gs0428 <lider28@naver.com>
2023-12-24 01:53:17 +0900

    feat: BottomTabNavigator option 추가

--- ReactNativeStudy/Lenny/Week4/mapApp/src/navigator/BottomTabNavigator.tsx
+++ ReactNativeStudy/Lenny/Week4/mapApp/src/navigator/BottomTabNavigator.tsx
@@ -2,13 +2,35 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import FavoriteScreen from '../screens/FavoriteScreen';
+import SvgIcons from '../components/SvgIcons';
+import TabBarTexts from '../components/TabBarTexts';
 
 export default function BottomTabNavigator() {
   const Tab = createBottomTabNavigator();
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Favorite" component={FavoriteScreen} />
+    <Tab.Navigator screenOptions={{headerShown: false}}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: SvgIcons.HomeIcon,
+          tabBarLabel: TabBarTexts.HomeText,
+          ...tabBarOption,
+        }}
+      />
+      <Tab.Screen
+        name="Favorite"
+        component={FavoriteScreen}
+        options={{
+          tabBarIcon: SvgIcons.StarIcon,
+          tabBarLabel: TabBarTexts.StarText,
+          ...tabBarOption,
+        }}
+      />
     </Tab.Navigator>
   );
 }
+
+const tabBarOption = {
+  tabBarItemStyle: {paddingVertical: 5},
+};
