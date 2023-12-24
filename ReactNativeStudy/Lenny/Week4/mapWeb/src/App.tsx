a87cf0b0e42ba30cac61a1b048d1be4e09d030b7
gs0428 <lider28@naver.com>
2023-12-25 01:52:30 +0900

    feat: App으로부터 받은 위치 이용하여 지도에 마커 찍기

--- ReactNativeStudy/Lenny/Week4/mapWeb/src/App.tsx
+++ ReactNativeStudy/Lenny/Week4/mapWeb/src/App.tsx
@@ -1,16 +1,57 @@
 import { useEffect } from "react";
 import "./App.css";
 
+declare global {
+  interface Window {
+    ReactNativeWebView: {
+      postMessage: (data: string) => void;
+    };
+  }
+}
+
+interface CoordinationProps {
+  coords: {
+    latitude: number;
+    longitude: number;
+  };
+}
+
 export default function App() {
-  useEffect(() => {
-    const center: naver.maps.LatLng = new naver.maps.LatLng(37.5666103, 126.9783882);
+  const initMap = (latitude: number, longitude: number) => {
+    const center: naver.maps.LatLng = new naver.maps.LatLng(latitude, longitude);
     const map: naver.maps.Map = new naver.maps.Map("map", {
       center: center,
+      zoomControl: true,
       zoom: 16,
     });
 
+    const marker = new naver.maps.Marker({
+      position: new naver.maps.LatLng(latitude, longitude),
+      map: map,
+    });
+
     map;
-  }, []);
+    marker;
+  };
+
+  // web으로 확인할 때 사용
+  // useEffect(() => {
+  //   navigator.geolocation.getCurrentPosition(
+  //     (e) => initMap(e.coords.latitude, e.coords.longitude),
+  //     (err) => console.log(err)
+  //   );
+  // }, []);
+
+  // react-native와 통신할 때 사용
+  if (window.ReactNativeWebView) {
+    window.addEventListener("message", (e) => {
+      const currentLocation: CoordinationProps = JSON.parse(e.data);
+      const { latitude: currentLatitude, longitude: currentLongitude } = currentLocation.coords;
+      window.ReactNativeWebView.postMessage(`${currentLatitude}, ${currentLongitude}`);
+      initMap(currentLatitude, currentLongitude);
+    });
+    window.removeEventListener("message", (e) => console.log(e));
+  }
 
   return <div id="map"></div>;
 }
