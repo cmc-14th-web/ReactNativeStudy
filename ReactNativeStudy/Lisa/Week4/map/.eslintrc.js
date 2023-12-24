e0bf72b05b6cb6c0d811a7d4f308f0ad53a4be6a
ghdtjgus76 <ghdtjgus76@naver.com>
2023-12-24 23:32:51 +0900

    feat: husky, lint-staged 세팅

--- ReactNativeStudy/Lisa/Week4/map/.eslintrc.js
+++ ReactNativeStudy/Lisa/Week4/map/.eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  parser: '@babel/eslint-parser',
+  parserOptions: {requireConfigFile: 'false'},
+  babelOptions: {configFile: './.babelrc'},
 };
