2b7b3a92a388a881e2dc224495054c6453a0a0cf
ghdtjgus76 <ghdtjgus76@naver.com>
2023-12-25 16:06:08 +0900

    feat: palette, typo 세팅

--- ReactNativeStudy/Lisa/Week4/map/src/styles/palette.ts
+++ ReactNativeStudy/Lisa/Week4/map/src/styles/palette.ts
@@ -1,1 +1,10 @@
-export const palette = {};
+export const palette = {
+  blue_100: '#E9EEFF',
+  blue_200: '#BFC9FF',
+  blue_600: '#5061FF',
+  gray_100: '#E4E4E4',
+  gray_200: '#C3C3C3',
+  gray_600: '#888888',
+  white: '#FFFFFF',
+  black: '#000000',
+};
--- ReactNativeStudy/Lisa/Week4/map/src/styles/typo.ts
+++ ReactNativeStudy/Lisa/Week4/map/src/styles/typo.ts
@@ -1,3 +1,11 @@
 import {StyleSheet} from 'react-native';
 
-export const typoStyles = {typo: StyleSheet.create({})};
+export const typoStyles = {
+  typo: StyleSheet.create({
+    body_1: {
+      fontSize: 12,
+      fontWeight: '400',
+      lineHeight: 16.8,
+    },
+  }),
+};
