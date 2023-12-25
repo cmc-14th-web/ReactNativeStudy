56c31bff146317292353bb4e0a45d40a9e3eda50
ghdtjgus76 <ghdtjgus76@naver.com>
2023-12-25 16:56:58 +0900

    feat: 위치 권한 관련 유틸 함수 작성 및 적용

--- ReactNativeStudy/Lisa/Week4/map/App.tsx
+++ ReactNativeStudy/Lisa/Week4/map/App.tsx
@@ -1,8 +1,14 @@
 import {SafeAreaView, StyleSheet} from 'react-native';
+import {useEffect} from 'react';
 
+import {checkLocationPermissions} from './src/utils/checkLocationPermissions';
 import Navigation from './src/navigators/Navigation';
 
 function App(): JSX.Element {
+  useEffect(() => {
+    checkLocationPermissions();
+  }, []);
+
   return (
     <SafeAreaView style={appStyles.container}>
       <Navigation />
---
+++ ReactNativeStudy/Lisa/Week4/map/src/utils/checkLocationPermissions.ts
@@ -0,0 +1,46 @@
+import {
+  checkMultiple,
+  request,
+  requestMultiple,
+} from 'react-native-permissions';
+import {permissions} from '../constants/permission';
+import {Alert} from 'react-native';
+
+export const checkLocationPermissions = () => {
+  checkMultiple([
+    permissions.coarseLocationPermission,
+    permissions.fineLocationPermission,
+  ])
+    .then(result => {
+      const coarseLocationPermissionStatus =
+        result[permissions.coarseLocationPermission];
+      const fineLocationPermissionStatus =
+        result[permissions.fineLocationPermission];
+
+      if (
+        coarseLocationPermissionStatus === 'denied' &&
+        fineLocationPermissionStatus === 'denied'
+      ) {
+        requestMultiple([
+          permissions.coarseLocationPermission,
+          permissions.fineLocationPermission,
+        ]);
+      } else if (coarseLocationPermissionStatus === 'denied') {
+        request(permissions.coarseLocationPermission)
+          .then(result => console.log(result))
+          .catch(error => console.log(error));
+      } else if (fineLocationPermissionStatus === 'denied') {
+        request(permissions.fineLocationPermission)
+          .then(result => console.log(result))
+          .catch(error => console.log(error));
+      }
+
+      if (
+        coarseLocationPermissionStatus === 'blocked' ||
+        fineLocationPermissionStatus === 'blocked'
+      ) {
+        Alert.alert('위치 권한을 허용해주세요.');
+      }
+    })
+    .catch(error => console.log(error));
+};
