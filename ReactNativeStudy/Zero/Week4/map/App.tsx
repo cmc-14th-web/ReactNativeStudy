ed487a6df37a8e3c97dd0dfbaf182906c2abf3ad
ShinYoung-Kim <lukey0515@naver.com>
2023-12-26 00:55:14 +0900

    feat: 웹뷰에 지도 url 연결

--- ReactNativeStudy/Zero/Week4/map/App.tsx
+++ ReactNativeStudy/Zero/Week4/map/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { SafeAreaView } from 'react-native';
 import WebView from 'react-native-webview';
 
+import { MAP_WEB_VIEW_LINK } from './src/constant/link';
+
 function App(): React.JSX.Element {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <WebView source={{ uri: 'https://www.naver.com' }} />
+      <WebView source={{ uri: MAP_WEB_VIEW_LINK }} />
     </SafeAreaView>
   );
 }
