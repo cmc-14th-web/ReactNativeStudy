e129587989ae7bd28889a85aa2634f802b956fb2
ghdtjgus76 <ghdtjgus76@naver.com>
2023-12-25 16:05:21 +0900

    feat: svg 컴포넌트 생성

---
+++ ReactNativeStudy/Lisa/Week4/map/src/components/SvgIcons/index.tsx
@@ -0,0 +1,29 @@
+import {Path, Svg, SvgProps} from 'react-native-svg';
+import {KeyOfPalette, theme} from '../../styles/theme';
+import {svgIconPath} from '../../constants/svgIconPath';
+
+interface SvgIconsProps extends SvgProps {
+  iconVariant: keyof typeof svgIconPath;
+  width?: number;
+  height?: number;
+  fill?: KeyOfPalette;
+}
+
+const SvgIcons = ({
+  iconVariant,
+  width = 24,
+  height = 24,
+  fill = 'blue_600',
+  ...props
+}: SvgIconsProps) => {
+  return (
+    <Svg {...props} width={width} height={height} viewBox="0 0 24 24">
+      <Path
+        d={svgIconPath[iconVariant]}
+        fill={theme.palette[fill]}
+      />
+    </Svg>
+  );
+};
+
+export default SvgIcons;
