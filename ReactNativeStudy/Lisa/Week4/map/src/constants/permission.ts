4d0107455fe8467cb827067d7fac877f92b0ad48
ghdtjgus76 <ghdtjgus76@naver.com>
2023-12-27 00:39:12 +0900

    refactor: permissions 타입 개선

--- ReactNativeStudy/Lisa/Week4/map/src/constants/permission.ts
+++ ReactNativeStudy/Lisa/Week4/map/src/constants/permission.ts
@@ -3,4 +3,4 @@ import {PERMISSIONS} from 'react-native-permissions';
 export const permissions = {
   coarseLocationPermission: PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION,
   fineLocationPermission: PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
-};
+} as const;
