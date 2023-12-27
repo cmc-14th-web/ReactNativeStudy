999514d0cb16a736c5f783888646cc22b742db2b
gs0428 <lider28@naver.com>
2023-12-27 15:51:33 +0900

    feat: 공통 container 생성

---
+++ ReactNativeStudy/Lenny/Week4/mapApp/src/components/Container.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import palette from '../styles/palette';
+
+export default function Container({children}: {children: React.ReactNode}) {
+  return <SafeAreaView style={style.container}>{children}</SafeAreaView>;
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
