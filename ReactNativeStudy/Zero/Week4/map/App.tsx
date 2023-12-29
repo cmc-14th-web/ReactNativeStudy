52e6bd9bd572df076de7e097f62c2ef3850aa8ba
ShinYoung-Kim <lukey0515@naver.com>
2023-12-29 15:13:25 +0900

    feat: app에 navigation 적용

--- ReactNativeStudy/Zero/Week4/map/App.tsx
+++ ReactNativeStudy/Zero/Week4/map/App.tsx
@@ -1,39 +1,14 @@
-import React, { useEffect, useRef } from 'react';
-import { PermissionsAndroid, SafeAreaView } from 'react-native';
-import WebView from 'react-native-webview';
-import Geolocation from 'react-native-geolocation-service';
-
-import { MAP_WEB_VIEW_LINK } from './src/constant/link';
+import { SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import BottomNavigation from './src/navigation/BottomNavigation';
 
 function App(): React.JSX.Element {
-  const mapRef = useRef<any>();
-
-  useEffect(() => {
-    PermissionsAndroid.requestMultiple([
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
-    ]);
-  }, []);
-
-  const postPostion = (position: Geolocation.GeoPosition) => {
-    mapRef.current.postMessage(JSON.stringify({
-      ...position.coords,
-      type: 'map',
-    }));
-  }
-
-  const handleMessage = () => {
-    Geolocation.getCurrentPosition((position) => {
-      postPostion(position);
-    });
-  }
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <WebView source={{ uri: MAP_WEB_VIEW_LINK }}
-        ref={mapRef}
-        onLoadEnd={handleMessage}
-      />
+      <NavigationContainer>
+        <BottomNavigation />
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
