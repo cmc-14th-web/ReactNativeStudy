75621df5eaf590001fb7793a798c3cbbf88740f5
ShinYoung-Kim <lukey0515@naver.com>
2023-12-29 15:12:28 +0900

    chore: icon, 색 상수 저장

---
+++ ReactNativeStudy/Zero/Week4/map/src/constant/color.ts
@@ -0,0 +1,12 @@
+export const COLOR = {
+  Blue100: '#E9EEFF',
+  Blue200: '#BFC9FF',
+  Blue600: '#5061FF',
+  White: '#FFFFFF',
+  Gray100: '#E4E4E4',
+  Gray200: '#C3C3C3',
+  Gray600: '#888888',
+  Black: '#000000',
+};
+
+export type ColorType = keyof typeof COLOR;
