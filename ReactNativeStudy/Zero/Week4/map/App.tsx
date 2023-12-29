55cb30ef40c5b880c7b7ccf8c0c61b0b5f53c92a
ShinYoung-Kim <lukey0515@naver.com>
2023-12-29 22:49:38 +0900

    feat: recoil root 추가

--- ReactNativeStudy/Zero/Week4/map/App.tsx
+++ ReactNativeStudy/Zero/Week4/map/App.tsx
@@ -1,14 +1,17 @@
 import { SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomNavigation from './src/navigation/BottomNavigation';
+import { RecoilRoot } from 'recoil';
 
 function App(): React.JSX.Element {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <NavigationContainer>
-        <BottomNavigation />
-      </NavigationContainer>
+      <RecoilRoot>
+        <NavigationContainer>
+          <BottomNavigation />
+        </NavigationContainer>
+      </RecoilRoot>
     </SafeAreaView>
   );
 }
